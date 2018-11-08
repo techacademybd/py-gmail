fadf50e891336912ddb54d558ca3397356888b16
Soumic Shekhar <soumic.aoyon@northsouth.edu>
2018-11-08 15:18:23 +0600

    added trigger.py

---
+++ codes/trigger.py.py
@@ -0,0 +1,18 @@
+import datetime
+import os
+
+
+# input time in hours, minutes and seconds...
+checker = "15:14:30"
+
+while 1:
+    date_time = datetime.datetime.now()
+    d = date_time.strftime("%m-%d-%Y")
+    t = date_time.strftime("%H:%M:%S")
+
+    if t == checker:
+        print("Starting new script....")
+        os.system("post-email.py 1")
+        break;
+
+
