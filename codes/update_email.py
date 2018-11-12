0e2b0d55ec747101a3a21ebdce312af7d91605b8
Soumic Shekhar <soumic.aoyon@northsouth.edu>
2018-11-12 16:11:26 +0600

    added update_email.py

--- codes/update_email.py
+++ codes/update_email.py
@@ -1,7 +1,10 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 import imaplib
 import email
 
-# Works in Python 2.7 only
+'''Read the latest email and updates the message content in google sheets
+'''
 
 # sender email and password
 FROM_EMAIL = "techacademy1234@gmail.com"
@@ -39,14 +42,43 @@ latest_email_id = id_list[-1]
 # get raw email
 _, data = mail.fetch(latest_email_id, "(RFC822)")
 raw_email = data[0][1]
-email_message = email.message_from_string(raw_email)
+
+# APIs are used
+scope = ['http://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+
+# keep secret
+cred_file = 'Sheets Test2-316f8201c13e.json'  # --->>>> techacademy1234@gmail.com
+
+credentials = ServiceAccountCredentials.from_json_keyfile_name(cred_file, scope)
+
+# authorize
+gc = gspread.authorize(credentials)
+
+SOURCE_EMAIL_ADDRESS = "techacademy1234@gmail.com"
+PASSWORD = "TESTtta1234"
+
+# open ss
+worksheet = gc.open('test')
+
+# main order cart
+wks_1 = worksheet.get_worksheet(5)
+# wks_1 = worksheet.worksheet("Sheet 1")
+
+decoded_raw_email = raw_email.decode()
+email_message = email.message_from_string(decoded_raw_email)
 
 # convert raw email to text
 message = get_first_text_block(email_message)
 
-print("Sent from: " + str(email.utils.parseaddr(email_message['From'])[0]))
+print("Sent from: " + str(email.utils.parseaddr(email_message['From'])[1]))
 print("\n")
 print("Email subject: " + str(email_message['Subject']))
 print("\n")
 print("Message: " + str(message))
 
+'''Put logic here:
+    If name is in column then update message block in that column
+'''
+
+for i in range(2):
+    wks_1.update_cell(i+2, 2, message)
